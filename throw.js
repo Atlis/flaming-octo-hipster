9b8d507875ddc5ab6f386107401e7bc375edf108
David <david.vallee.1@gmail.com>
2014-04-28 17:26:59 -0400

    third commit

--- throw.js
+++ throw.js
@@ -3,11 +3,6 @@ var fs = require('fs');
 var app     = express();
 
 app.get('/scrape', function(req, res){
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        console.log('File successfully written! - Check your project directory for the output.json file');
-    })
-    
-    res.send('Check your console!')
     var content;
     // First I want to read the file
     fs.readFile('output.json', function read(err, data) {
