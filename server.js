da325034a96294d653ada3632b0c70c47d475fe3
David <david.vallee.1@gmail.com>
2014-04-28 15:47:07 -0400

    third commit

--- server.js
+++ server.js
@@ -5,18 +5,13 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
-	
 	url = 'http://www.kijiji.ca/b-apartments-condos/ottawa/c37l1700185';
-
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
-
 			var json = {};
-
 			var dateTime = new Date();
-            dateTime = dateTime.toISOString();
-            
+            dateTime = dateTime.toLocaleString();
             var data_214 = $("a[data-id~='214']").parent().text().match(/\(([^)]+)\)/)[1].replace(",","");
             var data_212 = $("a[data-id~='212']").parent().text().match(/\(([^)]+)\)/)[1].replace(",","");
             var data_211 = $("a[data-id~='211']").parent().text().match(/\(([^)]+)\)/)[1].replace(",","");
@@ -24,15 +19,10 @@ app.get('/scrape', function(req, res){
             var data_213 = $("a[data-id~='213']").parent().text().match(/\(([^)]+)\)/)[1].replace(",","");
             var data_216 = $("a[data-id~='216']").parent().text().match(/\(([^)]+)\)/)[1].replace(",","");
             json.data = [dateTime, data_214, data_212, data_211, data_215, data_213, data_216];
-            
 		}
-        
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-
+        fs.appendFile('output.json', json.data, function(err){
+        	console.log('Data saved.');
         })
-        
         res.send('Check your console!')
 	})
 })
