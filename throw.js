f9fc30fa2239b22a5bd023d964dbbf37abdf8343
David <david.vallee.1@gmail.com>
2014-05-04 21:02:32 -0400

    object

--- throw.js
+++ throw.js
@@ -4,7 +4,7 @@ var app     = express();
 
 app.get('/scrape', function(req, res){
     var content;
-    fs.readFile('/root/log.txt', function read(err, data) {
+    fs.readFile('/root/obj.txt', function read(err, data) {
         if (err) throw err;
         res.header("Content-Type", "text/plain");
         res.send(data);
