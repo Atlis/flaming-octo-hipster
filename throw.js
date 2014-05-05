60cc80b80da32dc95ea5da718e99b6076b9a3236
David <david.vallee.1@gmail.com>
2014-05-04 20:57:14 -0400

    object

--- throw.js
+++ throw.js
@@ -4,13 +4,13 @@ var app     = express();
 
 app.get('/scrape', function(req, res){
     var content;
-    fs.readFile('/root/obj.txt', function read(err, data) {
+    fs.readFile('/root/output.json', function read(err, data) {
         if (err) throw err;
         res.header("Content-Type", "text/plain");
         res.send(data);
     }); 
 })
 
-app.listen('8081');
+app.listen('8081')
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
