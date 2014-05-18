0558a304e54e4884a8feeb3656c385af08030ea6
David <david.vallee.1@gmail.com>
2014-05-18 19:54:59 -0400

    json

--- throw.js
+++ throw.js
@@ -4,7 +4,7 @@ var app     = express();
 
 app.get('/scrape', function(req, res){
     var content;
-    fs.readFile('/root/ads-ville-de-montreal.txt', function read(err, data) {
+    fs.readFile('./data/ads-ville-de-montreal.js', function read(err, data) {
         if (err) throw err;
         res.header("Content-Type", "text/plain");
         res.send(data);
